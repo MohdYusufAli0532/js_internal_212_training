ccc95ef7c7c6a8edc8b931a16ff7b08d77133c69
Mohd Yusuf Ali <mohd0532yusuf@gmail.com>
2025-12-09 05:31:30 +0000

    Day 2 Training

---
+++ Day2/variable.js
@@ -0,0 +1,58 @@
+//1st Task
+
+// let a=23
+// const b="abc"
+// var c=78
+// d=88
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+
+
+
+
+
+
+//2nd Task
+
+let a = "abc"
+let b=""
+let c=null
+let d;
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+console.log("Types:")
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
+
+
+//3rd Task
+
+// let score=33;
+// console.log(typeof(score))
+// console.log(typeof score)
+
+
+let score="33abc";
+console.log(typeof(score))
+console.log(typeof score)
+
+//type cast
+let x=Number(score);
+
+console.log(typeof(x))
+console.log(typeof x)
+
+console.log(x);
