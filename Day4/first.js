50a00f92dfb5e6f9a7a6e45cad814033dead9d54
Mohd Yusuf Ali <mohd0532yusuf@gmail.com>
2025-12-11 05:30:39 +0000

    Day4 training done

---
+++ Day4/first.js
@@ -0,0 +1,27 @@
+//Date functions
+
+let myDate=new Date();
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.getDate());
+
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(myDate.toTimeString());
+
+
+console.log(myDate.toUTCString());
+
